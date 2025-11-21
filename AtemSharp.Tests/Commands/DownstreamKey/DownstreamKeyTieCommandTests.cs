c80d466c84cc7336011d5fc6d46cd3bc987ef17f
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-21 21:06:07 +0100

    add CDsT

---
+++ AtemSharp.Tests/Commands/DownstreamKey/DownstreamKeyTieCommandTests.cs
@@ -0,0 +1,26 @@
+using AtemSharp.Commands.DownstreamKey;
+using AtemSharp.State;
+
+namespace AtemSharp.Tests.Commands.DownstreamKey;
+
+[TestFixture]
+public class DownstreamKeyTieCommandTests : SerializedCommandTestBase<DownstreamKeyTieCommand, DownstreamKeyTieCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public byte Index { get; set; }
+        public bool Tie { get; set; }
+    }
+
+    protected override DownstreamKeyTieCommand CreateSut(TestCaseData testCase)
+    {
+        return new DownstreamKeyTieCommand(new DownstreamKeyer
+        {
+            Id = testCase.Command.Index,
+            Properties =
+            {
+                Tie = testCase.Command.Tie
+            }
+        });
+    }
+}
---
+++ AtemSharp/Commands/DownstreamKey/DownstreamKeyTieCommand.cs
@@ -0,0 +1,16 @@
+using AtemSharp.Helpers;
+using AtemSharp.State;
+
+namespace AtemSharp.Commands.DownstreamKey;
+
+[Command("CDsT")]
+[BufferSize(4)]
+public partial class DownstreamKeyTieCommand(DownstreamKeyer keyer) : SerializedCommand
+{
+    [SerializedField(0)]
+    [NoProperty]
+    private readonly byte _downStreamKeyerId = keyer.Id;
+
+    [SerializedField(1)]
+    private bool _tie = keyer.Properties.Tie;
+}
