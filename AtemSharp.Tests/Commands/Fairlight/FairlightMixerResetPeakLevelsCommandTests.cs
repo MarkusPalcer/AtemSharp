4720129b9c52a09409919e48fb8cb4653c9ad423
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-22 22:14:07 +0100

    add RFLP

---
+++ AtemSharp.Tests/Commands/Fairlight/FairlightMixerResetPeakLevelsCommandTests.cs
@@ -0,0 +1,23 @@
+using AtemSharp.Commands.Fairlight;
+
+namespace AtemSharp.Tests.Commands.Fairlight;
+
+[TestFixture]
+public class FairlightMixerResetPeakLevelsCommandTests : SerializedCommandTestBase<FairlightMixerResetPeakLevelsCommand,
+    FairlightMixerResetPeakLevelsCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public bool All { get; set; }
+        public bool Master { get; set; }
+    }
+
+    protected override FairlightMixerResetPeakLevelsCommand CreateSut(TestCaseData testCase)
+    {
+        return new FairlightMixerResetPeakLevelsCommand
+        {
+            All = testCase.Command.All,
+            Master = testCase.Command.Master,
+        };
+    }
+}
---
+++ AtemSharp/Commands/Fairlight/FairlightMixerResetPeakLevelsCommand.cs
@@ -0,0 +1,37 @@
+using AtemSharp.Enums;
+using AtemSharp.Lib;
+
+namespace AtemSharp.Commands.Fairlight;
+
+[Command("RFLP")]
+public class FairlightMixerResetPeakLevelsCommand : SerializedCommand
+{
+    public bool All { get; set; }
+    public bool Master { get; set; }
+
+    public override byte[] Serialize(ProtocolVersion version)
+    {
+        var buffer = new byte[4];
+        byte val = 0;
+
+        if (All)
+        {
+            val |= 1 << 0;
+
+            // some magic number that is needed for this to work
+            buffer.WriteUInt8(0x01, 1);
+        }
+
+        if (Master)
+        {
+            val |= 1 << 1;
+
+            // some magic number that is needed for this to work
+            buffer.WriteUInt8(0x04, 3);
+        }
+
+        buffer.WriteUInt8(val, 0);
+
+        return buffer;
+    }
+}
