f961b38ca44537fa109b59456e772a878bf49dd8
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-23 21:44:23 +0100

    add CMPC

---
+++ AtemSharp.Tests/Commands/Media/MediaPoolClearClipCommandTests.cs
@@ -0,0 +1,17 @@
+using AtemSharp.Commands.Media;
+using AtemSharp.State;
+
+namespace AtemSharp.Tests.Commands.Media;
+
+public class MediaPoolClearClipCommandTests : SerializedCommandTestBase<MediaPoolClearClipCommand, MediaPoolClearClipCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public byte Index { get; set; }
+    }
+
+    protected override MediaPoolClearClipCommand CreateSut(TestCaseData testCase)
+    {
+        return new MediaPoolClearClipCommand(new MediaPoolEntry { Id = testCase.Command.Index });
+    }
+}
--- AtemSharp/State/MediaPoolEntry.cs
+++ AtemSharp/State/MediaPoolEntry.cs
@@ -3,7 +3,6 @@ namespace AtemSharp.State;
 public class MediaPoolEntry
 {
     public byte Id { get; set; }
-
     public bool IsUsed { get; set; }
     public string Hash { get; set; } = string.Empty;
     public string FileName { get; set; } = string.Empty;
