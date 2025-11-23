7599475e248a8aeddba09f8e8dcba8cc21701ce4
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-23 22:47:10 +0100

    add DAut

---
+++ AtemSharp.Tests/Commands/MixEffects/Transition/AutoTransitionCommandTests.cs
@@ -0,0 +1,17 @@
+using AtemSharp.Commands.MixEffects.Transition;
+using AtemSharp.State;
+
+namespace AtemSharp.Tests.Commands.MixEffects.Transition;
+
+public class AutoTransitionCommandTests : SerializedCommandTestBase<AutoTransitionCommand, AutoTransitionCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public byte Index { get; set; }
+    }
+
+    protected override AutoTransitionCommand CreateSut(TestCaseData testCase)
+    {
+        return new AutoTransitionCommand(new MixEffect { Index = testCase.Command.Index });
+    }
+}
