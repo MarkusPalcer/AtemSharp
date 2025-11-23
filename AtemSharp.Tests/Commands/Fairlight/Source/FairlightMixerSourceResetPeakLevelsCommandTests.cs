cb1092f97a444db46d51884685f37455c6e1e5fa
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-23 12:17:19 +0100

    add RFIP

---
+++ AtemSharp.Tests/Commands/Fairlight/Source/FairlightMixerSourceResetPeakLevelsCommandTests.cs
@@ -0,0 +1,29 @@
+using AtemSharp.Commands.Fairlight.Source;
+
+namespace AtemSharp.Tests.Commands.Fairlight.Source;
+
+public class FairlightMixerSourceResetPeakLevelsCommandTests : SerializedCommandTestBase<FairlightMixerSourceResetPeakLevelsCommand, FairlightMixerSourceResetPeakLevelsCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public ushort Index { get; set; }
+        public long SourceId { get; set; }
+        public bool Output { get; set; }
+        public bool DynamicsInput { get; set; }
+        public bool DynamicsOutput { get; set; }
+    }
+
+    protected override FairlightMixerSourceResetPeakLevelsCommand CreateSut(TestCaseData testCase)
+    {
+        return new FairlightMixerSourceResetPeakLevelsCommand(new AtemSharp.State.Audio.Fairlight.Source
+        {
+            Id = testCase.Command.SourceId,
+            InputId = testCase.Command.Index
+        })
+        {
+            Output = testCase.Command.Output,
+            DynamicsInput = testCase.Command.DynamicsInput,
+            DynamicsOutput = testCase.Command.DynamicsOutput,
+        };
+    }
+}
