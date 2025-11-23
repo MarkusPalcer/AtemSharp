1b61d8b64d3c6d2674afbafad249fa8c751cc89b
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-23 20:16:38 +0100

    add MSRc

---
+++ AtemSharp.Tests/Commands/Macro/MacroRecordCommandTests.cs
@@ -0,0 +1,23 @@
+using AtemSharp.Commands.Macro;
+
+namespace AtemSharp.Tests.Commands.Macro;
+
+public class MacroRecordCommandTests : SerializedCommandTestBase<MacroRecordCommand, MacroRecordCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public ushort Index { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+
+    protected override MacroRecordCommand CreateSut(TestCaseData testCase)
+    {
+        return new MacroRecordCommand(new AtemSharp.State.Macro
+        {
+            Id = testCase.Command.Index,
+            Name = testCase.Command.Name,
+            Description = testCase.Command.Description,
+        });
+    }
+}
---
+++ AtemSharp/Commands/Macro/MacroRecordCommand.cs
@@ -0,0 +1,27 @@
+using AtemSharp.Commands;
+using AtemSharp.Enums;
+using AtemSharp.Lib;
+
+namespace AtemSharp.Commands.Macro;
+
+// Manual serialization due to variable buffer size
+[Command("MSRc")]
+public class MacroRecordCommand(AtemSharp.State.Macro targetSlot) : SerializedCommand
+{
+    private ushort _index = targetSlot.Id;
+
+    public string Name { get; set; } = targetSlot.Name;
+    public string Description { get; set; } = targetSlot.Description;
+
+    public override byte[] Serialize(ProtocolVersion version)
+    {
+        var buffer = new byte[AtemUtil.PadToMultiple(8 + Name.Length + Description.Length, 4)];
+        buffer.WriteUInt16BigEndian(_index, 0);
+        buffer.WriteUInt16BigEndian((ushort)Name.Length, 2);
+        buffer.WriteUInt16BigEndian((ushort)Description.Length, 4);
+        buffer.WriteString(Name, 6);
+        buffer.WriteString(Description, 6 + Name.Length);
+
+        return buffer;
+    }
+}
