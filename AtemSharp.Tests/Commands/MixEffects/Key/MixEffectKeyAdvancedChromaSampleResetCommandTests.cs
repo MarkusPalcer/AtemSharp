f40018ddfdba57c4e1215f98be1ad769561d44dd
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-24 08:12:51 +0100

    add RACK

---
+++ AtemSharp.Tests/Commands/MixEffects/Key/MixEffectKeyAdvancedChromaSampleResetCommandTests.cs
@@ -0,0 +1,30 @@
+using AtemSharp.Commands.MixEffects.Key;
+using AtemSharp.State;
+
+namespace AtemSharp.Tests.Commands.MixEffects.Key;
+
+public class MixEffectKeyAdvancedChromaSampleResetCommandTests : SerializedCommandTestBase<MixEffectKeyAdvancedChromaSampleResetCommand, MixEffectKeyAdvancedChromaSampleResetCommandTests.CommandData>
+{
+    public class CommandData : CommandDataBase
+    {
+        public byte MixEffectIndex  { get; set; }
+        public byte KeyerIndex { get; set; }
+        public bool ChromaCorrection { get; set; }
+        public bool ColorAdjustments { get; set; }
+        public bool KeyAdjustments { get; set; }
+    }
+
+    protected override MixEffectKeyAdvancedChromaSampleResetCommand CreateSut(TestCaseData testCase)
+    {
+        return new MixEffectKeyAdvancedChromaSampleResetCommand(new UpstreamKeyer
+        {
+            Id = testCase.Command.KeyerIndex,
+            MixEffectId = testCase.Command.MixEffectIndex,
+        })
+        {
+            ResetKeyAdjustments = testCase.Command.KeyAdjustments,
+            ResetChromaCorrection = testCase.Command.ChromaCorrection,
+            ResetColorAdjustments =  testCase.Command.ColorAdjustments
+        };
+    }
+}
---
+++ AtemSharp/Commands/MixEffects/Key/MixEffectKeyAdvancedChromaSampleResetCommand.cs
@@ -0,0 +1,25 @@
+using AtemSharp.Helpers;
+using AtemSharp.Lib;
+using AtemSharp.State;
+
+namespace AtemSharp.Commands.MixEffects.Key;
+
+[Command("RACK")]
+[BufferSize(4)]
+public partial class MixEffectKeyAdvancedChromaSampleResetCommand(UpstreamKeyer keyer) :SerializedCommand
+{
+    [SerializedField(0)][NoProperty] private byte _mixEffectId = keyer.MixEffectId;
+    [SerializedField(1)] [NoProperty] private byte _id = keyer.Id;
+    [CustomSerialization] private bool _resetKeyAdjustments;
+    [CustomSerialization] private bool _resetChromaCorrection;
+    [CustomSerialization] private bool _resetColorAdjustments;
+
+    private void SerializeInternal(byte[] buffer)
+    {
+        byte val = 0;
+        if (_resetKeyAdjustments) val |= 1 << 0;
+        if (_resetChromaCorrection) val |= 1 << 1;
+        if (_resetColorAdjustments) val |= 1 << 2;
+        buffer.WriteUInt8(val, 3);
+    }
+}
