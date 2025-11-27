c071751fae2abe8fd48b1aa0696483e447f5e7ed
Markus Palcer <MasterOfVDL@GMail.com>
2025-11-27 15:43:24 +0100

    switch TransitionWipeUpdateCommand to code generation
    Closes #38

--- AtemSharp/Commands/MixEffects/Transition/TransitionWipeUpdateCommand.cs
+++ AtemSharp/Commands/MixEffects/Transition/TransitionWipeUpdateCommand.cs
@@ -1,5 +1,4 @@
 using AtemSharp.State;
-using AtemSharp.State.Info;
 
 namespace AtemSharp.Commands.MixEffects.Transition;
 
@@ -7,83 +6,67 @@ namespace AtemSharp.Commands.MixEffects.Transition;
 /// Command received from ATEM device containing wipe transition settings update
 /// </summary>
 [Command("TWpP")]
-public class TransitionWipeUpdateCommand : IDeserializedCommand
+public partial class TransitionWipeUpdateCommand : IDeserializedCommand
 {
     /// <summary>
     /// Mix effect index (0-based)
     /// </summary>
-    public int MixEffectId { get; init; }
+    [DeserializedField(0)] private byte _mixEffectId;
 
     /// <summary>
     /// Rate of the wipe transition in frames
     /// </summary>
-    public byte Rate { get; init; }
+    [DeserializedField(1)] private byte _rate;
 
     /// <summary>
     /// Pattern for the wipe transition
     /// </summary>
-    public byte Pattern { get; init; }
+    [DeserializedField(2)] private byte _pattern;
 
     /// <summary>
     /// Width of the wipe border as percentage (0-100%)
     /// </summary>
-    public double BorderWidth { get; init; }
+    [DeserializedField(4)] [SerializedType(typeof(ushort))] [ScalingFactor(100)]
+    private double _borderWidth;
 
     /// <summary>
     /// Input source for the wipe border
     /// </summary>
-    public ushort BorderInput { get; init; }
+    [DeserializedField(6)] private ushort _borderInput;
 
     /// <summary>
     /// Symmetry setting for the wipe transition as percentage (0-100%)
     /// </summary>
-    public double Symmetry { get; init; }
+    [DeserializedField(8)] [SerializedType(typeof(ushort))] [ScalingFactor(100)]
+    private double _symmetry;
 
     /// <summary>
     /// Softness of the wipe border as percentage (0-100%)
     /// </summary>
-    public double BorderSoftness { get; init; }
+    [DeserializedField(10)] [SerializedType(typeof(ushort))] [ScalingFactor(100)]
+    private double _borderSoftness;
 
     /// <summary>
     /// X position for the wipe transition (0.0-1.0)
     /// </summary>
-    public double XPosition { get; init; }
+    [DeserializedField(12)] [SerializedType(typeof(ushort))] [ScalingFactor(10000)]
+    private double _xPosition;
 
     /// <summary>
     /// Y position for the wipe transition (0.0-1.0)
     /// </summary>
-    public double YPosition { get; init; }
+    [DeserializedField(14)] [SerializedType(typeof(ushort))] [ScalingFactor(10000)]
+    private double _yPosition;
 
     /// <summary>
     /// Whether the wipe direction is reversed
     /// </summary>
-    public bool ReverseDirection { get; init; }
+    [DeserializedField(16)] private bool _reverseDirection;
 
     /// <summary>
     /// Whether flip flop mode is enabled
     /// </summary>
-    public bool FlipFlop { get; init; }
-
-    /// <summary>
-    /// Deserialize the command from binary stream
-    /// </summary>
-    public static TransitionWipeUpdateCommand Deserialize(ReadOnlySpan<byte> rawCommand, ProtocolVersion protocolVersion)
-    {
-        return new TransitionWipeUpdateCommand
-        {
-            MixEffectId = rawCommand.ReadUInt8(0),
-            Rate = rawCommand.ReadUInt8(1),
-            Pattern = rawCommand.ReadUInt8(2),
-            BorderWidth = rawCommand.ReadUInt16BigEndian(4) / 100.0,
-            BorderInput = rawCommand.ReadUInt16BigEndian(6),
-            Symmetry = rawCommand.ReadUInt16BigEndian(8) / 100.0,
-            BorderSoftness = rawCommand.ReadUInt16BigEndian(10) / 100.0,
-            XPosition = rawCommand.ReadUInt16BigEndian(12) / 10000.0,
-            YPosition = rawCommand.ReadUInt16BigEndian(14) / 10000.0,
-            ReverseDirection = rawCommand.ReadBoolean(16),
-            FlipFlop = rawCommand.ReadBoolean(17)
-        };
-    }
+    [DeserializedField(17)] private bool _flipFlop;
 
     /// <inheritdoc />
     public void ApplyToState(AtemState state)
