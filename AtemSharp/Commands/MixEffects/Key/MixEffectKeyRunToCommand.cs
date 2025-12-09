d3a8a1446b9e70bd9f8fe9f1656745457c36dece
Markus Palcer <MasterOfVDL@GMail.com>
2025-12-09 21:50:21 +0100

    optimize static creation methods on MixEffectKeyRunToCommand

--- AtemSharp/Commands/MixEffects/Key/MixEffectKeyRunToCommand.cs
+++ AtemSharp/Commands/MixEffects/Key/MixEffectKeyRunToCommand.cs
@@ -12,7 +12,7 @@ public partial class MixEffectKeyRunToCommand : SerializedCommand
 
     [SerializedField(4)] [NoProperty] private readonly byte _keyframeId;
 
-    [SerializedField(5)] [NoProperty] private FlyKeyDirection _direction;
+    [SerializedField(5)] [NoProperty] private readonly FlyKeyDirection _direction;
 
     private MixEffectKeyRunToCommand(byte mixEffectId, byte keyerId, byte keyframeId, FlyKeyDirection direction)
     {
@@ -22,21 +22,33 @@ public partial class MixEffectKeyRunToCommand : SerializedCommand
         _direction = direction;
     }
 
-    // TODO #83: Capture test data for this case
     /// <summary>
-    /// Creates a new command that sends the keyer to "RunToInfinite"
+    /// Creates a new command that sends the keyer into the given direction until it is invisible
     /// </summary>
     public static MixEffectKeyRunToCommand RunToInfinite(UpstreamKeyer keyer, FlyKeyDirection direction)
     {
         return new MixEffectKeyRunToCommand(keyer.MixEffectId, keyer.Id, 4, direction);
     }
 
-    public static MixEffectKeyRunToCommand RunToFull(UpstreamKeyer keyer, FlyKeyDirection direction = FlyKeyDirection.CentreOfKey)
+    /// <summary>
+    /// Creates a new command that animates the keyer to full screen
+    /// </summary>
+    public static MixEffectKeyRunToCommand RunToFull(UpstreamKeyer keyer) => RunToFull(keyer, FlyKeyDirection.CentreOfKey);
+
+    // This exists because the tests insist on a direction to be serialized even if no direction makes sense in this case
+    internal static MixEffectKeyRunToCommand RunToFull(UpstreamKeyer keyer, FlyKeyDirection direction)
     {
         return new MixEffectKeyRunToCommand(keyer.MixEffectId, keyer.Id, 3, direction);
     }
 
-    public static MixEffectKeyRunToCommand RunToKeyframe(UpstreamKeyerFlyKeyframe keyframe, FlyKeyDirection direction = FlyKeyDirection.CentreOfKey)
+    /// <summary>
+    /// Creates a new command that animates the keyer to the given keyframe
+    /// </summary>
+    public static MixEffectKeyRunToCommand RunToKeyframe(UpstreamKeyerFlyKeyframe keyframe) =>
+        RunToKeyframe(keyframe, FlyKeyDirection.CentreOfKey);
+
+    // This exists because the tests insist on a direction to be serialized even if no direction makes sense in this case
+    internal static MixEffectKeyRunToCommand RunToKeyframe(UpstreamKeyerFlyKeyframe keyframe, FlyKeyDirection direction)
     {
         return new MixEffectKeyRunToCommand(keyframe.MixEffectId, keyframe.UpstreamKeyerId, keyframe.Id, direction);
     }
